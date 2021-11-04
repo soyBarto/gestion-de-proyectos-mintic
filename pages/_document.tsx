bb97390b22f5f7bd4bebfec0ee5ac9e8e6624f9e
soyBarto <jdtorresf@unal.edu.co>
2021-11-03 19:26:30 -0500

    Corregir build

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,25 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+/* eslint-disable @next/next/no-sync-scripts */
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-    static async getInitialProps(ctx) {
-        const initialProps = await Document.getInitialProps(ctx)
-        return { ...initialProps }
+    static async getInitialProps(ctx: any) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return { ...initialProps };
     }
 
     render() {
         return (
             <Html>
-                <Head><script src="https://kit.fontawesome.com/b88f783c54.js" crossorigin="anonymous"></script></Head>
+                <Head>
+                    <script src='https://kit.fontawesome.com/433667d0d8.js' crossOrigin='anonymous' />
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
                 </body>
             </Html>
-        )
+        );
     }
 }
 
-export default MyDocument
+export default MyDocument;
